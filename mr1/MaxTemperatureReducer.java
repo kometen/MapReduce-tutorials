05946e5b412291b636e938bb7dc23f3e82f60ec1
Claus Guttesen <kometen@gmail.com>
2011-10-13 22:02:10 +0200

    Set values in wm-object.

--- mr1/MaxTemperatureReducer.java
+++ mr1/MaxTemperatureReducer.java
@@ -13,10 +13,16 @@ import org.apache.hadoop.mapred.Reporter;
 public class MaxTemperatureReducer extends MapReduceBase implements Reducer<Text, Weather, Text, Text> {
 	public void reduce(Text key, Iterator<Weather> values, OutputCollector<Text, Text> output, Reporter reporter) throws IOException {
 		Weather w = new Weather();
-		Weather wm = new Weather();	// max. temp.
+		Weather wm = new Weather("1967", 10, 28, Integer.MIN_VALUE, "1");	// max. temp.
 		while (values.hasNext()) {
 			w = (Weather) values.next();
-			wm = (wm.getTemperature() > w.getTemperature()) ? wm : w;
+			if (w.getTemperature() > wm.getTemperature()) {
+				wm.setYear(w.getYear());
+				wm.setMonth(w.getMonth());
+				wm.setDay(w.getDay());
+				wm.setTemperature(w.getTemperature());
+				wm.setQuality(w.getQuality());
+			}
 		}
 		String out = wm.getYear() + "," + Integer.toString(wm.getMonth()) + "," + Integer.toString(wm.getDay()) + "," + Integer.toString(wm.getTemperature());
 		output.collect(key, new Text(out));
