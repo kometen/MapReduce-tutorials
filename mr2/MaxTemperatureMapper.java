7fc71feb1085d3a5e142f22d56f926687fe53564
Claus Guttesen <kometen@gmail.com>
2011-10-24 10:34:57 +0200

    Exclude irrelevant lines

--- mr2/MaxTemperatureMapper.java
+++ mr2/MaxTemperatureMapper.java
@@ -15,15 +15,17 @@ public class MaxTemperatureMapper extends MapReduceBase implements Mapper<LongWr
 	
 	public void map(LongWritable key, Text value, OutputCollector<Text, Weather> output, Reporter reporter) throws IOException {
 		String line = value.toString();
-		String year = line.substring(14,18);
-		int month = Integer.parseInt(line.substring(18,20));
-		int day = Integer.parseInt(line.substring(20,22));
-		String stringTemp = line.substring(24,30);
-		int airTemperature = (int)(Double.parseDouble(stringTemp.trim()) * 10);
-		String quality = "0";
-		if (airTemperature != MISSING && quality.matches("[01459]")) {
-			Weather w = new Weather(year, month, day, airTemperature, quality);
-			output.collect(new Text(year), w);
+		if (!line.startsWith("STN---")) {
+			String year = line.substring(14,18);
+			int month = Integer.parseInt(line.substring(18,20));
+			int day = Integer.parseInt(line.substring(20,22));
+			String stringTemp = line.substring(24,30);
+			int airTemperature = (int)(Double.parseDouble(stringTemp.trim()) * 10);
+			String quality = "0";
+			if (airTemperature != MISSING && quality.matches("[01459]")) {
+				Weather w = new Weather(year, month, day, airTemperature, quality);
+				output.collect(new Text(year), w);
+			}
 		}
 	}
 }
